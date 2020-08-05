8e15f43f823ceff1e6bd5da8e36a4730e6f51973
Robbert Verbruggen <rfverbruggen@me.com>
2020-08-05 07:56:41 +0200

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,12 +3,15 @@ import unittest
 
 
 class CoreTestCase(unittest.TestCase):
+    """Core test case."""
 
     def setUp(self):
+        """set up function."""
         self.app = bridg.create_app()
         self.app.testing = True
         self.app = self.app.test_client()
 
     def test_home(self):
+        """Test the home page."""
         result = self.app.get('/')
         assert result.data == b"Hello World!"
